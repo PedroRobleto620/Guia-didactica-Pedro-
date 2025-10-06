cfc39790e3e3e3c17992c7b8493e3fc0c3c992a1
PedroRobleto620 <darklink620@gmail.com>
2025-10-06 01:57:43 -0600

    primer commit de guia 1 de Pedro Robleto

---
+++ prisma/migrations/20251006042033_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "Password" TEXT NOT NULL,
+    "Telephone" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER'
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
